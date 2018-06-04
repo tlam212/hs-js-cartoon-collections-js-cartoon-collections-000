ed43f102f932149317d2e6e062293a452e2fbb8a
Toni Lamont <tlam212@gmail.com>
2018-06-04 22:14:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+  let dwarfNames = [];
+  for (let i = 0; i<dwarves.length; i++) {
+    dwarfNames.push(`${i+1}. ${dwarves[i]} `); 
+  }
+   return dwarfNames.join("");
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.map(element =>
+  element.toUpperCase() + "!")
 }
 
 function longPlaneteerCalls(words) {
+  let result = words.some(function(word){
+    return word.length > 4;
+  }); 
+  return result;
 }
 
 function findTheCheese (foods) {
+  let result = foods.filter(function(food){
+    return food === "cheddar" ||
+    food === "gouda" ||
+    food === "Camembert";
+  });
+  if(result.length !== 0) {
+    return result;
+  }else{
+    return "no cheese!";
+  }
 }
+
+
